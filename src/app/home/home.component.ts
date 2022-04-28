7ba4846303ceb298af7e1857243c323163ffade4
cdasilvaccsf <claudiadasilva@Claudias-MacBook-Air.local>
2022-04-28 16:05:06 -0700

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MydataService } from '../mydata.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  members: any;
+
+  constructor(private mydata:MydataService) { }
+
+  ngOnInit(): void {
+    this.mydata.getData().subscribe(mydata => {
+      this.members = mydata
+      console.log(this.members)
+    })
+  }
+}
